110add63476d99fc1e5201705146bc2790af1f70
Arvind Kumar <arvindkum8312@gmail.com>
2025-01-05 13:39:15 +0530

    logo updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           to="/"
           className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex"
         >
-          <img src="../public/logo.png" alt="Logo" className="h-8 w-8" />
+          <img src="../public/Logo.png" alt="Logo" className="h-8 w-8" />
           <span className="hidden sm:inline-block">Ecommerce</span>
         </Link>
 
